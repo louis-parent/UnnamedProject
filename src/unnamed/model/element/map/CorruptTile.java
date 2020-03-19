b85413bd55c23ce3a53e952fc173a371128f7ccc
Appixroid <appixroid@gmail.com>
2020-03-19 20:23:06 +0100

    Adding corrupt tile

---
+++ src/unnamed/model/element/map/CorruptTile.java
@@ -0,0 +1,27 @@
+package unnamed.model.element.map;
+
+import org.newdawn.slick.SlickException;
+
+import unnamed.model.PixelisedImage;
+import unnamed.model.container.ElementContainer;
+
+public class CorruptTile extends Tile
+{
+	private static TileImageRegistry corruptRegistry;
+
+	public static void init() throws SlickException
+	{
+		CorruptTile.corruptRegistry = new TileImageRegistry("corrupt");
+	}
+
+	public CorruptTile(int column, int row, TileType type, ElementContainer container)
+	{
+		super(column, row, type, container);
+	}
+
+	@Override
+	public PixelisedImage getSprite()
+	{
+		return CorruptTile.corruptRegistry.get(CorruptTile.corruptRegistry.getImageNameFor(this.getType(), this.getSpriteVariant()));
+	}
+}
